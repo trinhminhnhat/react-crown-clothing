a024e965940702a62b8d97941863a6a3ac36ec7e
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-03-20 07:00:00 +0700

    style: base UI

--- src/App.js
+++ src/App.js
@@ -1,42 +1,35 @@
-import './categories.styles.scss';
+import Directory from 'components/Directory';
 
 const App = () => {
     const categories = [
         {
             id: 1,
             title: 'Hats',
+            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         },
         {
             id: 2,
             title: 'Jackets',
+            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
         },
         {
             id: 3,
             title: 'Sneakers',
+            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
         },
         {
             id: 4,
             title: 'Women',
+            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         },
         {
             id: 5,
             title: 'Men',
+            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         },
     ];
 
-    return (
-        <div className="categories-container">
-            {categories.map(({ id, title }) => (
-                <div key={id} className="category-container">
-                    {/* <img src="" alt="" /> */}
-                    <div className="category-body-container">
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            ))}
-        </div>
-    );
+    return <Directory categories={categories} />;
 };
 
 export default App;
