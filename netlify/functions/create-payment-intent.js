aaeccdd2b6f0ac862fb8a2063f8352d8d5134380
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-04-26 07:00:00 +0700

    feat: write severless function use with netlify

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+    try {
+        const { amount } = JSON.parse(event.body);
+        const paymentIntent = await stripe.paymentIntent.createPaymentIntent({
+            amount,
+            currency: 'usd',
+            payment_method_types: ['card'],
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ paymentIntent }),
+        };
+    } catch (error) {
+        console.log('Payment error: ', error);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error }),
+        };
+    }
+};
