702a9a65723cd8fe51d90337ff53e341e19c65f5
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-05-08 20:00:00 +0700

    feat: optimize using dynamic import

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
 // import SHOP_DATA from 'mock-data/shop-data';
-import Authentication from 'routes/Authentication';
-import Checkout from 'routes/Checkout';
-import Home from 'routes/Home';
-import Navigation from 'routes/Navigation';
-import Shop from 'routes/Shop';
+import Spinner from 'components/Spinner/Spinner';
 import { checkUserSession } from 'store/user/user.action';
 // import { addCollectionAndDocuments } from 'utils/firebase';
+
+const Home = lazy(() => import('routes/Home'));
+const Navigation = lazy(() => import('routes/Navigation'));
+const Checkout = lazy(() => import('routes/Checkout'));
+const Shop = lazy(() => import('routes/Shop'));
+const Authentication = lazy(() => import('routes/Authentication'));
 
 const App = () => {
     const dispatch = useDispatch();
@@ -20,14 +22,16 @@ const App = () => {
     }, []);
 
     return (
-        <Routes>
-            <Route path="/" element={<Navigation />}>
-                <Route index element={<Home />} />
-                <Route path="shop/*" element={<Shop />} />
-                <Route path="auth" element={<Authentication />} />
-                <Route path="checkout" element={<Checkout />} />
-            </Route>
-        </Routes>
+        <Suspense fallback={<Spinner />}>
+            <Routes>
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Home />} />
+                    <Route path="shop/*" element={<Shop />} />
+                    <Route path="auth" element={<Authentication />} />
+                    <Route path="checkout" element={<Checkout />} />
+                </Route>
+            </Routes>
+        </Suspense>
     );
 };
 
--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import { CartItem as TCartItem } from 'store/cart/cart.type';
 import { CardItemContainer, ItemDetail } from './cart-item.styles';
@@ -7,7 +7,7 @@ type CardItemProps = {
     cartItem: TCartItem;
 }
 
-const CartItem: FC<CardItemProps> = ({ cartItem }) => {
+const CartItem: FC<CardItemProps> = memo(({ cartItem }) => {
     const { imageUrl, price, name, quantity } = cartItem;
 
     return (
@@ -21,6 +21,6 @@ const CartItem: FC<CardItemProps> = ({ cartItem }) => {
             </ItemDetail>
         </CardItemContainer>
     );
-};
+});
 
 export default CartItem;
