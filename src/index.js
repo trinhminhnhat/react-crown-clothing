2e9d12bf518d354e0875f5cdcbb3f52a7636f447
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-05-09 20:00:00 +0700

    feat: migrate to progressive web app

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { persistor, store } from 'store/store';
 import { stripePromise } from 'utils/stripe';
@@ -25,6 +26,8 @@ root.render(
         </Provider>
     </React.StrictMode>,
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
